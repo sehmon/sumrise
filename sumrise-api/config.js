fe8b5b642678254ddb4ae57cbdc77cfe12ef9696
yawkity <sehmon3@gmail.com>
2016-03-21 13:30:33 -0400

    Updated config with secret (lol bad idea, I know) and database urls

--- sumrise-api/config.js
+++ sumrise-api/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secret': 'Fl74|L=huYSVI1&',
-    'database': 'mongodb://sumriseusername:sumrisepassword@ds021289.mlab.com:21289/sumrise"
+    'database': 'mongodb://localhost:27017/sumrise',
     'apiKey': '72a6d4f554b22646985d5abeb1cf7a11',
     'appId': 'cd9a38f7'
 }
