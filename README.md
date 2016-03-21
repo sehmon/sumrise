0e186d08fa312193155e747bcef9370c838ffb22
yawkity <sehmon3@gmail.com>
2016-03-21 12:18:51 -0400

    Uploading server to digital ocean

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Sumrise
 Link summarizer and indexer - CapitalOne Tech Summit 2016
+---
+So I come across a lot of articles that interest me on a daily basis. I really don't have a way of saving/indexing them, so I created this application to solve that problem. This app is a link sumamrizer and indexer: When you find an article you like you can save it to your own private journal, along with a summarized version of the articles contents for easy viewing later on. 
+
+This is full application, with user authenticaiton and http requests to a server I created to handle the needs of someone using this app. There were plans for an Android Client to view your summaries, but because of finals I don't know if that'll happen haha.
+
+Technologies:
+1. Server - Node.js and Express
+2. Chrome Extension - HTML, CSS, and JS
+3. Android Client - Java
--- sumrise-chrome-extension/popup.js
+++ sumrise-chrome-extension/popup.js
@@ -1,3 +1,5 @@
+var baseUrl = "http://localhost:8000/"
+
 function getCurrentTabUrl(callback) {
     //Query tabs
     var queryInfo = {
@@ -6,8 +8,69 @@ function getCurrentTabUrl(callback) {
     };
 
     //Queries the chrome API for the tab then returns it's url
-    chrome.tabs.query(queryInfo, funciton(tabs) {
+    chrome.tabs.query(queryInfo, function(tabs) {
         var tab = tabs[0];
         callback(tab.url);
     });
 };
+
+function fillUrlField(url){
+    document.getElementById('urlText').value = url;
+}
+
+function sendApiRequest(url, callback, errorCallback){
+    console.log("Sending API request with url: " + url);
+    var http = new XMLHttpRequest();
+    var apiUrl = baseUrl+"api/article";
+    // var params = "url=" + url;
+
+    $.post(apiUrl, {
+        url: url
+    }, function(data, status){
+        alert("Data: " + data + "\nStatus: " + status);
+    });
+
+    // http.responseType = 'json';
+    // http.onload = function(e){
+    //     var response = http.response;
+    //     if(!response){
+    //         errorCallback("No response from API");
+    //         return;
+    //     } 
+    //     console.log(response);
+    //     callback(response);
+    // }
+    // http.onerror = function(e){
+    //     console.log("Error: " + e);
+    // }
+    //
+    // http.open("POST", apiUrl, true);
+    // http.send(params);
+}
+
+function alertMe(){
+    alert("Alerts working");
+}
+
+function submitSummary(){
+    console.log("Submitting Summary");
+    url = document.getElementById('urlText').value;
+
+    sendApiRequest(url, function(response){
+        console.log(response);
+    }, function(error){
+        console.log(error);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    getCurrentTabUrl(function(url){
+        fillUrlField(url);
+    });
+
+    var submit = document.getElementById('submitButton');
+    submit.addEventListener('click', function(){
+        submitSummary();
+    });
+});
+
